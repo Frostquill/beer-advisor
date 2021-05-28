6d5cc1f37106d14551e9729e6544f849dae15646
Mike M <mcewball13@gmail.com>
2021-05-28 13:45:35 -0700

    added basic MVP functionality

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -2,25 +2,42 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 34.0523523, lng: -118.2435731 },
     zoom: 8,
   });
 }
+let startingInput, destinationInput, waypointInput;
+let waypnts = [];
 
 // Calculate and render direction on the map
 const renderDirectionOnMap = (origin, destination) => {
-  let directionService = new google.maps.DirectionService(),
-    directionRenderer = new google.maps.DirectionRenderer(),
+  let directionService = new google.maps.DirectionsService(),
+    directionRenderer = new google.maps.DirectionsRenderer(),
     request = {
       origin: origin,
       destination: destination,
+      waypoints: waypnts,
       travelMode: "DRIVING",
     };
-    directionRenderer.setMap(map);
-    directionService.route(request, (result, status) => {
-        if (status == "OK") {
-            directionRenderer.setDirections(result);
-        }
-    })
+  directionRenderer.setMap(map);
+  directionService.route(request, (result, status) => {
+    if (status == "OK") {
+      directionRenderer.setDirections(result);
+    }
+  });
 };
+$("#waypointBtn").on("click", function () {
+  waypointInput = $("#waypointInput").val();
+  console.log("click worked");
+  waypnts.push({
+    location: waypointInput,
+    stopover: true,
+  });
+});
+$("#searchBtn").on("click", function () {
+  startingInput = $("#startingInput").val();
+  destinationInput = $("#destinationInput").val();
 
+  console.log(startingInput, destinationInput);
+  renderDirectionOnMap(startingInput, destinationInput);
+});
