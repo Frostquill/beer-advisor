2dab208059852af19acdfe2d8a8904c5b827c04a
Mike M <mcewball13@gmail.com>
2021-05-28 11:27:41 -0700

    initial map functionality

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -6,3 +6,21 @@ function initMap() {
     zoom: 8,
   });
 }
+
+// Calculate and render direction on the map
+const renderDirectionOnMap = (origin, destination) => {
+  let directionService = new google.maps.DirectionService(),
+    directionRenderer = new google.maps.DirectionRenderer(),
+    request = {
+      origin: origin,
+      destination: destination,
+      travelMode: "DRIVING",
+    };
+    directionRenderer.setMap(map);
+    directionService.route(request, (result, status) => {
+        if (status == "OK") {
+            directionRenderer.setDirections(result);
+        }
+    })
+};
+
